4bb6db779189b5fd2f10f156a7799907be780329
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2024-08-11 22:32:21 -0600

    Chapter 05

---
+++ ch05email/src/main/java/com/rose/ch05email/data/UserDB.java
@@ -0,0 +1,16 @@
+package com.rose.ch05email.data;
+
+import com.rose.ch05email.business.User;
+
+/**
+ *
+ * @author rose
+ */
+public class UserDB {
+
+    public static long insert(User user) {
+        // TODO: Add code that adds the user to the database
+        // NOTE: This is shown in chapters 11-13
+        return 0;
+    }
+}
