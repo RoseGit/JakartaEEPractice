fe4c1c68954c0d6916a5322e7b2510f2d062db47
Rose Garcia Cruz <ing.rosalio.garcia.cruz@gmail.com>
2024-08-16 13:23:26 -0600

    Chapter 07 Cart

---
+++ ch07cart/src/main/java/com/rose/data/ProductIO.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rose.data;
+
+import com.rose.business.Product;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+/**
+ *
+ * @author Rose
+ */
+public class ProductIO {
+
+    public static Product getProduct(String code, String filepath) {
+        try {
+            File file = new File(filepath);
+            BufferedReader in = new BufferedReader(
+                    new FileReader(file));
+
+            String line = in.readLine();
+            while (line != null) {
+                StringTokenizer t = new StringTokenizer(line, "|");
+                String productCode = t.nextToken();
+                if (code.equalsIgnoreCase(productCode)) {
+                    String description = t.nextToken();
+                    double price = Double.parseDouble(t.nextToken());
+                    Product p = new Product();
+                    p.setCode(code);
+                    p.setDescription(description);
+                    p.setPrice(price);
+                    in.close();
+                    return p;
+                }
+                line = in.readLine();
+            }
+            in.close();
+            return null;
+        } catch (IOException e) {
+            System.err.println(e);
+            return null;
+        }
+    }
+
+    public static ArrayList<Product> getProducts(String filepath) {
+        ArrayList<Product> products = new ArrayList<Product>();
+        File file = new File(filepath);
+        try {
+            BufferedReader in = new BufferedReader(
+                    new FileReader(file));
+
+            String line = in.readLine();
+            while (line != null) {
+                StringTokenizer t = new StringTokenizer(line, "|");
+                String code = t.nextToken();
+                String description = t.nextToken();
+                String priceAsString = t.nextToken();
+                double price = Double.parseDouble(priceAsString);
+                Product p = new Product();
+                p.setCode(code);
+                p.setDescription(description);
+                p.setPrice(price);
+                products.add(p);
+                line = in.readLine();
+            }
+            in.close();
+            return products;
+        } catch (IOException e) {
+            System.err.println(e);
+            return null;
+        }
+    }
+}
