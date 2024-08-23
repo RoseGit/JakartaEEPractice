e39986fdb41aafdf69551c245e5e6cd1b75fb8ca
Rose Garcia Cruz <ing.rosalio.garcia.cruz@gmail.com>
2024-08-23 12:54:42 -0600

    Chapter 08

---
+++ ch08email/src/main/java/com/rose/data/UserDB.java
@@ -0,0 +1,25 @@
+/*
+ */
+package com.rose.data;
+
+import com.rose.business.*;
+
+/**
+ * Responsible for registering the user
+ *
+ * @author Rose
+ */
+public class UserDB {
+
+     /**
+     * Allows you to register a user
+     *
+     * @param user @see {@link User}
+     * @return User Id.
+     */
+    public static long insert(User user) {
+        // TODO: Add code that adds the user to the database
+        // NOTE: This is shown in chapters 11-13
+        return 0;
+    }
+}
