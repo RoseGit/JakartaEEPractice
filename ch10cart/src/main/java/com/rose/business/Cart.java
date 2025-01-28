8ba138f5b45f55cf404b439a7b9fa8c3401fd221
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2025-01-27 23:57:11 -0600

    Chapter 10

---
+++ ch10cart/src/main/java/com/rose/business/Cart.java
@@ -0,0 +1,58 @@
+package com.rose.business;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Cart implements Serializable
+{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -5243020756859507729L;
+	private ArrayList<LineItem> items;
+    
+    public Cart()
+    {
+        items = new ArrayList<LineItem>();
+    }
+    
+    public ArrayList<LineItem> getItems()
+    { 
+        return items;
+    }
+    
+    public int getCount()
+    { 
+        return items.size();
+    }
+    
+    public void addItem(LineItem item)
+    {
+        String code = item.getProduct().getCode();
+        int quantity = item.getQuantity();
+        for (int i = 0; i < items.size(); i++)
+        {
+            LineItem lineItem = items.get(i);
+            if (lineItem.getProduct().getCode().equals(code))
+            {
+                lineItem.setQuantity(quantity);
+                return;
+            }
+        }
+        items.add(item);
+    }
+    
+    public void removeItem(LineItem item)
+    {
+        String code = item.getProduct().getCode();
+        for (int i = 0; i < items.size(); i++)
+        {
+            LineItem lineItem = items.get(i);
+            if (lineItem.getProduct().getCode().equals(code))
+            {
+                items.remove(i);
+                return;
+            }
+        }
+    }
+}
