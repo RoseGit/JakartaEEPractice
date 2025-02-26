ec8e6e2d803be04d37bb29f61a4110d99558ec09
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2025-02-25 19:23:42 -0600

    webapp configuration

--- ch09email/src/main/java/com/rose/business/User.java
+++ ch09email/src/main/java/com/rose/business/User.java
@@ -10,7 +10,11 @@ import java.io.Serializable;
 
 public class User implements Serializable {
 
-    private String firstName;
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String firstName;
     private String lastName;
     private String email;
 
